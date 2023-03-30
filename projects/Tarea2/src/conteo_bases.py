e96e67098316b113d3935dea70d83d9190baa9a0
StochasticMike12 <mfvarela@lcg.unam.mx>
2023-03-29 23:13:39 -0600

    Programa de tarea 2 añadido

---
+++ projects/Tarea2/src/conteo_bases.py
@@ -0,0 +1,29 @@
+'''
+NAME
+     posicion_codones.py 
+
+VERSION
+        1.0
+
+AUTHOR
+        Miguel Ángel Flores Varela
+
+DESCRIPTION
+        Este programa arroja ciertas posiciones en especpifico dentro de una secuencia de DNA.
+
+CATEGORY
+        Genómica
+
+USAGE
+
+    % py posicion_codones.py
+    
+    
+'''
+
+# ===========================================================================
+# =                            main
+# ===========================================================================
+
+
+# step 1.A=input()
