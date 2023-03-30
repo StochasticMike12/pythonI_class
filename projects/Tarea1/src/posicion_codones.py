dc4dae5d5c4016c07c1e692e2b1b2f893ccae59c
StochasticMike12 <mfvarela@lcg.unam.mx>
2023-03-29 22:40:37 -0600

    Programa de tarea 1 añadido

---
+++ projects/Tarea1/src/posicion_codones.py
@@ -0,0 +1,39 @@
+
+'''
+NAME
+     posicion_codones.py 
+
+VERSION
+        1.0
+
+AUTHOR
+        Miguel Ángel Flores Varela
+
+DESCRIPTION
+        Este programa arroja ciertas posiciones en especpifico dentro de una secuencia de DNA.
+
+CATEGORY
+        Genómica
+
+USAGE
+
+    % py posicion_codones.py
+    
+    
+'''
+
+# ===========================================================================
+# =                            main
+# ===========================================================================
+
+
+# step 1.
+
+# Guardar la secuencia de DNA, el codón de inicio y el codón de final en una variable cada uno.
+secuencia="AAGGTACGTCGCGCGTTATTAGCCTAAT"
+c_inicio="TAC"
+c_termino="ATT"
+
+# Imprimir la secuencia, la posición donde comienza el codón de inicio y la posición donde termina el codón de terminación.
+print("\nSecuencia de DNA: AAGGTACGTCGCGCGTTATTAGCCTAAT\n\nPosición del codon de inicio:", secuencia.find(c_inicio), "\n\nPosición donde termina el codón de paro: ", secuencia.find(c_termino)+2, "\n\nFragmento que será RNA es: TACGTCGCGCGTT\n")
+
