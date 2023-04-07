15e26820c5a2676195aa9839fd4d123c4c5ca1f3
StochasticMike12 <mfvarela@lcg.unam.mx>
2023-04-06 22:29:26 -0600

    Salto de línea removido en dna.txt

--- projects/Tarea3/src/contenido_AG_CG.py
+++ projects/Tarea3/src/contenido_AG_CG.py
@@ -1,6 +1,6 @@
 '''
 NAME
-     posicion_codones.py 
+     contenido_AT_CG.py 
 
 VERSION
         1.0
@@ -9,14 +9,14 @@ AUTHOR
         Miguel Ángel Flores Varela
 
 DESCRIPTION
-        Este programa arroja ciertas posiciones en especpifico dentro de una secuencia de DNA.
+        Este programa pide la ruta de un archivo con una secuencia de DNA y arroja el procentaje de AT y GC de la secuencia dada.
 
 CATEGORY
         Genómica
 
 USAGE
 
-    % py posicion_codones.py
+    % py contenido_AT_CG.py
     
     
 '''
@@ -27,3 +27,15 @@ USAGE
 
 
 # step
+
+print("\nIntroduce la ruta del archivo con la secuencia de DNA: ", end=" ")
+ruta_archivo = input()
+archivo = open(ruta_archivo, "r")
+dna = archivo.read()
+longitud_dna = len(dna)
+AT = dna.count("A")+dna.count("T")
+GC = dna.count("G")+dna.count("C")
+porcentaje_AT = (100*AT)/longitud_dna
+porcentaje_GC = (100*GC)/longitud_dna
+print("\n\n\nArchivo de secuencia:",ruta_archivo, "\n\nPorcentaje de AT y GC:\n\nAT:", porcentaje_AT, "\n\nGC:", porcentaje_GC, "\n\n\n")
+archivo.close()
