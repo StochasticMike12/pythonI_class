18f6c2a453b211226286b195ef470dd3955c82b0
StochasticMike12 <mfvarela@lcg.unam.mx>
2023-09-11 19:46:19 -0600

    Cambio a programas.

--- gato/src/gato.py
+++ gato/src/gato.py
@@ -44,3 +44,24 @@ USAGE
 # =                            main
 # ===========================================================================
 
+class gato():
+    vertebrado : True
+    mamifero : True
+
+    def __init__(self, color_de_ojos, pelaje, color_de_pelo, patron_pelaje):
+        self.color_de_ojos = color_de_ojos
+        self.pelaje = pelaje
+        self.color_de_pelo : color_de_pelo
+        self.patron_pelaje : patron_pelaje
+
+    def maullar(self):
+        print("Miauuuu")
+    
+    def ronronear(self):
+        print("Rrrrrrrrrr")
+    
+    def prmear(self):
+        print("Pr")
+
+class gato_egipcio(gato):
+    
